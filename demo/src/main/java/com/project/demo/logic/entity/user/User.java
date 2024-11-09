69113e30d8415cc6723ce68c07776c63dbf1d83f
paulahhe <129335393+paulahhe@users.noreply.github.com>
2024-11-08 17:13:13 -0800

    Order delete
    Eliminación de ordenes

--- demo/src/main/java/com/project/demo/logic/entity/user/User.java
+++ demo/src/main/java/com/project/demo/logic/entity/user/User.java
@@ -1,5 +1,4 @@
 package com.project.demo.logic.entity.user;
-import com.project.demo.logic.entity.order.Order;
 import com.project.demo.logic.entity.rol.Role;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -43,9 +42,6 @@ public class User implements UserDetails {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "role_id", referencedColumnName = "id", nullable = false)
     private Role role;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
-    private List<Order> orders;
 
     // Constructors
     public User() {}
@@ -137,13 +133,6 @@ public class User implements UserDetails {
         return role;
     }
 
-    public List<Order> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
-    }
 
     public User setRole(Role role) {
         this.role = role;
