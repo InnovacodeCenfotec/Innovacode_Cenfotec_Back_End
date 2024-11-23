3f5c944e3b55dfd6a93defa4648a42deec1da9ef
adrDev0 <165166065+adrDev0@users.noreply.github.com>
2024-11-23 11:40:29 -0600

    interference with google fixed

--- demo/src/main/java/com/project/demo/rest/cloudinary/ImageRestController.java
+++ demo/src/main/java/com/project/demo/rest/cloudinary/ImageRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/images")
+@RequestMapping("/cloudinary")
 public class ImageRestController {
 
     @Autowired
