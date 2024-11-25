bce834e01c62d10278c2ccd4d95a2b97ca8f41ea
adrDev0 <165166065+adrDev0@users.noreply.github.com>
2024-11-25 11:03:52 -0600

    Atributo de fecha de creación agregado a imagen

--- demo/src/main/java/com/project/demo/logic/entity/cloudinary/Image.java
+++ demo/src/main/java/com/project/demo/logic/entity/cloudinary/Image.java
@@ -2,6 +2,10 @@ package com.project.demo.logic.entity.cloudinary;
 
 import com.project.demo.logic.entity.user.User;
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
 
 @Table(name = "images")
 @Entity
@@ -15,6 +19,10 @@ public class Image {
 
     @Column(name = "url_image")
     private String url;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "create_date")
+    private Date createDate;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
@@ -42,6 +50,14 @@ public class Image {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
     }
 
     public User getUser() {
